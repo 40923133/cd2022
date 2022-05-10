af71c8d3edab948e38acf5082f782fe9a21a2435
40923133 <40923133@gm.nfu.edu.tw>
2022-05-11 01:05:28 +0800

    w11

---
+++ wink files/w11.js
@@ -0,0 +1,29 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 633, y: 534, width: 60, height: 24 }, target: 3 },
+    ],
+    "44": [
+      { type: "gotoframe", rect: { x: 452, y: 201, width: 60, height: 24 }, target: 45 },
+    ],
+    "79": [
+      { type: "gotoframe", rect: { x: 571, y: 196, width: 60, height: 24 }, target: 80 },
+    ],
+    "89": [
+      { type: "gotoframe", rect: { x: 579, y: 159, width: 60, height: 24 }, target: 90 },
+    ],
+    "143": [
+      { type: "gotoframe", rect: { x: 579, y: 390, width: 60, height: 24 }, target: 144 },
+    ],
+    "188": [
+      { type: "gotoframe", rect: { x: 579, y: 328, width: 60, height: 24 }, target: 189 },
+    ],
+    "208": [
+      { type: "gotoframe", rect: { x: 526, y: 237, width: 60, height: 24 }, target: 209 },
+    ],
+  },
+};
