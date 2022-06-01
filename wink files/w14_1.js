05770531088ec1d3ee786c402cdf5219e3fe30b6
40923133 <40923133@gm.nfu.edu.tw>
2022-06-01 13:44:29 +0800

    w14

---
+++ wink files/w14_1.js
@@ -0,0 +1,27 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 911, y: 451, width: 42, height: 19 }, target: 3 },
+    ],
+    "7": [
+      { type: "gotoframe", rect: { x: 494, y: 149, width: 42, height: 19 }, target: 2 },
+      { type: "gotoframe", rect: { x: 540, y: 147, width: 42, height: 19 }, target: 8 },
+    ],
+    "28": [
+      { type: "gotoframe", rect: { x: 679, y: 414, width: 42, height: 19 }, target: 7 },
+      { type: "gotoframe", rect: { x: 722, y: 414, width: 42, height: 19 }, target: 29 },
+    ],
+    "49": [
+      { type: "gotoframe", rect: { x: 798, y: 378, width: 42, height: 19 }, target: 28 },
+      { type: "gotoframe", rect: { x: 842, y: 377, width: 42, height: 19 }, target: 50 },
+    ],
+    "69": [
+      { type: "gotoframe", rect: { x: 251, y: 450, width: 42, height: 19 }, target: 49 },
+      { type: "gotoframe", rect: { x: 296, y: 451, width: 42, height: 19 }, target: 70 },
+    ],
+  },
+};
---
+++ wink files/w14_2.js
@@ -0,0 +1,27 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 587, y: 324, width: 60, height: 24 }, target: 3 },
+    ],
+    "35": [
+      { type: "gotoframe", rect: { x: 698, y: 395, width: 60, height: 24 }, target: 36 },
+      { type: "gotoframe", rect: { x: 635, y: 394, width: 60, height: 24 }, target: 2 },
+    ],
+    "41": [
+      { type: "gotoframe", rect: { x: 799, y: 344, width: 60, height: 24 }, target: 35 },
+      { type: "gotoframe", rect: { x: 865, y: 345, width: 60, height: 24 }, target: 42 },
+    ],
+    "66": [
+      { type: "gotoframe", rect: { x: 736, y: 549, width: 60, height: 24 }, target: 67 },
+      { type: "gotoframe", rect: { x: 672, y: 549, width: 60, height: 24 }, target: 41 },
+    ],
+    "280": [
+      { type: "gotoframe", rect: { x: 926, y: 326, width: 60, height: 24 }, target: 281 },
+      { type: "gotoframe", rect: { x: 861, y: 325, width: 60, height: 24 }, target: 66 },
+    ],
+  },
+};
