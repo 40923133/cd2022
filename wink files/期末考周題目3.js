2cdde9b510e469eabc9e27447407780ec26e370c
40923133 <40923133@gm.nfu.edu.tw>
2022-06-26 23:09:51 +0800

    期末考周題目2、3、4

---
+++ wink files/期末考周題目3.js
@@ -0,0 +1,19 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 10,
+  buttonFrameLength: 5,
+  buttonFrameOffset: 2,
+  frameStops: {
+    "2": [
+      { type: "gotoframe", rect: { x: 810, y: 209, width: 60, height: 24 }, target: 3 },
+    ],
+    "29": [
+      { type: "gotoframe", rect: { x: 890, y: 425, width: 60, height: 24 }, target: 30 },
+      { type: "gotoframe", rect: { x: 825, y: 425, width: 60, height: 24 }, target: 2 },
+    ],
+    "49": [
+      { type: "gotoframe", rect: { x: 597, y: 585, width: 60, height: 24 }, target: 50 },
+      { type: "gotoframe", rect: { x: 532, y: 586, width: 60, height: 24 }, target: 29 },
+    ],
+  },
+};
